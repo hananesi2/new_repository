2cf8c1da0f8b86ab36a91eb5c7ce7dc3a2c67f30
hananesi2 <hananeett1@gmail.com>
2023-03-08 13:19:30 +0100

    commit 1

---
+++ tests.py
@@ -0,0 +1,26 @@
+import calculator
+
+def test_add():
+    assert calculator.add(2, 3) == 5
+    assert calculator.add(-2, 3) == 1
+    assert calculator.add(2, -3) == -1
+
+def test_subtract():
+    assert calculator.subtract(2, 3) == -1
+    assert calculator.subtract(-2, 3) == -5
+    assert calculator.subtract(2, -3) == 5
+
+def test_multiply():
+    assert calculator.multiply(2, 3) == 6
+    assert calculator.multiply(-2, 3) == -6
+    assert calculator.multiply(2, -3) == -6
+
+def test_divide():
+    assert calculator.divide(6, 3) == 2
+    assert calculator.divide(-6, 3) == -2
+    assert calculator.divide(6, -3) == -2
+
+    try:
+        calculator.divide(6, 0)
+    except ValueError as e:
+        assert str(e) == "Cannot divide by zero!"
